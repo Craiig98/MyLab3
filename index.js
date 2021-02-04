ca508f39c1e61d187731b1bcdce0ac5707b14707
Craiig98 <craigleith98@gmail.com>
2021-02-04 17:47:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var http = require("http")
+var http = require("http");
+
+const PORT = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
 
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 
 	response.end('Hello World\n');
-}).listen(8080);
+}).listen(PORT);
 
 console.log('Server running at http://127.0.0.1:8080/');

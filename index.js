d2a70ea41f351b086cb5d3952ed023b72edc7a8a
Craiig98 <craigleith98@gmail.com>
2021-02-05 12:40:09 +0000

    Updated index.js file for html reading

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var http = require("http");
-
+fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
 
-	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.writeHead(200, {
+		'Content-Type': 'text/html',
+		'Access-Control-Allow-Origin' : '*'
+	});
+	var readStream = fs.createReadStream(__dirname + '/index.html');
 
-	response.end('Hello World\n');
+	readStream.pipe(response);
 }).listen(PORT);
 
 console.log('Server running at http://127.0.0.1:8080/');

7fa67412dc741976d7fdb51c82bf3b75262c5eb6
Craiig98 <craigleith98@gmail.com>
2021-02-05 17:56:34 +0000

    Rout middleware for parameters

--- server.js
+++ server.js
@@ -20,6 +20,17 @@ var adminRouter = express.Router();
  // continue doing what we were doing and go to the route
  next(); });
 
+ // route middleware to validate :name
+ adminRouter.param('name', function(req, res, next, name) {
+ // do validation on name here
+ // log something so we know its working
+ console.log('doing name validations on ' + name);
+ // once validation is done save the new item in the req
+ req.name = name;
+ // go to the next thing
+ next();
+ });
+
 // admin main page. the dashboard (http://localhost:PORT/admin)
 adminRouter.get('/', function(req, res) {
  res.send('I am the dashboard!'); });
@@ -32,7 +43,7 @@ adminRouter.get('/posts', function(req, res) {
 
 // route with parameters (http://localhost:PORT/admin/users/:name)
  adminRouter.get('/users/:name', function(req, res) {
- res.send('hello ' + req.params.name + '!'); });
+ res.send('hello ' + req.name + '!'); });
 
 // apply the routes to our application
 app.use('/admin', adminRouter);
